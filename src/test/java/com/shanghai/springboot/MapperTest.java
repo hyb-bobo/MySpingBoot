adad0ee580b53505f86637a2224868a03a8b6766
hyb-bobo <31762573+hyb-bobo@users.noreply.github.com>
2018-09-26 10:20:37 +0800

    mapper

--- src/test/java/com/shanghai/springboot/MapperTest.java
+++ src/test/java/com/shanghai/springboot/MapperTest.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RunWith(SpringRunner.class)
@@ -30,5 +31,27 @@ public class MapperTest {
     public void test1() {
         List<Tuser> users = userRepository.findAll();
         System.out.println(users);
+    }
+
+    public static void main(String[] args) {
+        List<Tuser> tusers = new ArrayList<Tuser>();
+        List<Tuser> tusersA = new ArrayList<Tuser>();
+        for (int i=0;i<10;i++){
+            Tuser u = new Tuser();
+            u.setName("aaaaa"+i*i);
+            u.setPassword("aaaaa"+i);
+            tusers.add(u);
+        }
+
+        for (int i=0;i<10;i++){
+            Tuser u = new Tuser();
+            u.setName("aaaaa"+i);
+            tusersA.add(u);
+        }
+
+        for(int j=0;j<10;j++){
+//            tusersA.get(j).getName().equals();
+
+        }
     }
 }
